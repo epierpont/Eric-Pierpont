87699200f404596439ddf4a94cc5c610678b3f41
Eric Pierpont <epierpont@swiftcom.com>
2018-05-23 21:17:25 -0400

    Icon updates and started recent projects section

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>Eric Pierpont | LAMP Developer / WordPress Monkey</title>
+        <title>Eric Pierpont && LAMP Developer && WordPress Monkey</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -36,7 +36,7 @@
 						<div class="container">
 
 							<div class="row">
-    						<div class="col-9">
+    						<div class="col-8">
 									<h1 class="text-right">Chuppta? I'm a <strong>LAMP Developer</strong><br/>AND <strong>WordPress Monkey</strong></h1>
 								</div><!-- END .col -->
 								<div class="col">
@@ -48,7 +48,28 @@
 					</div><!-- END #header-message -->
 				</header>
 
+				<section id="recent-projects">
+					<div class="container">
+						<div class="row">
+							<div class="col">
+								<h2 class="text-center">// recent <span>projects</span></h2>
+							</div><!-- END .col -->
+						</div>
+					</div><!-- END .container -->
 
+					<div class="row no-gutters">
+						<div class="col">
+							<div class="project steamboat">
+								<h3>Steamboat Today</h3>
+							</div>
+						</div>
+						<div class="col">
+							<div class="project summit">
+								<h3>Summit Daily</h3>
+							</div>
+						</div>
+					</div>
+				</section>
 
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
