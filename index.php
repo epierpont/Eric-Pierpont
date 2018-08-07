e08d9a49f0d829b6a81bc6f2cc209a01212bac49
Eric Pierpont <epierpont@swiftcom.com>
2018-08-07 08:05:57 -0600

    Progress made with project layout

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
         <!-- Place favicon.ico in the root directory -->
 
 				<link rel="stylesheet" href="css/vendor/bootstrap.min.css">
+				<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
         <link rel="stylesheet" href="css/main.css">
     </head>
     <body>
@@ -56,27 +57,22 @@
 						</div><!-- END .col -->
 					</div><!-- END .row -->
 
-					<div class="row no-gutters">
+					<div class="container">
 
-						<div class="col">
-							<div class="project news-site-theme">
-								<div class="image-container">
-									<div class="project-bg"></div>
-									<div class="project-image"><img src="http://ericpierpont.com/portfolio/img/pixel-news-site-theme.jpg" /></div>
-								</div><!-- END .image-container -->
-							</div><!-- END .project -->
-						</div><!-- END .col -->
+						<div class="row">
+							<div class="col-6 project">
+								<i class="fas fa-newspaper"></i>
+								<h3>WP News Theme <span>/ Swift Communications</span></h3>
+								<p>I am responsible for the co-creation and day-to-day maintenance of a custom OOP WordPress theme currently driving many popular mountain town news sites. The theme is powering over 15 sites such as <a href="#">Aspen Times</a>, <a href="#">Vail Daily</a>, <a href="#">Steamboat Today</a>, <a href="#">Tahoe Daily Tribune</a> and more.</p>
+							</div>
+							<div class="col-6 project">
+								<i class="fas fa-camera"></i>
+								<h3>WP Media Center Theme <span>/ Swift Communications</span></h3>
+								<p>News publications require a method to display content from their SmugMug account on a platform independent of the main site. This theme is running <a href="#">Park Record Photos</a> and <a href="#">Summit Daily Photos</a>.</p>
+							</div>
+						</div><!-- END .row -->
 
-						<div class="col">
-							<div class="project news-site-theme">
-								<div class="image-container">
-									<div class="project-bg"></div>
-									<div class="project-image"><img src="http://ericpierpont.com/portfolio/img/pixel-news-site-theme.jpg" /></div>
-								</div><!-- END .image-container -->
-							</div><!-- END .project -->
-						</div><!-- END .col -->
-
-					</div><!-- END .row -->
+					</div>
 
 				</section>
 
