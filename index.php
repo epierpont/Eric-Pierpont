80ce8be330c6330393cfb462b48c9abd338ed1b4
Eric Pierpont <epierpont@swiftcom.com>
2019-03-21 16:14:36 -0600

    Progress made with about section

--- index.php
+++ index.php
@@ -50,12 +50,12 @@
 					</div><!-- END #header-message -->
 				</header>
 
-				<section id="recent-projects">
+				<!--<section id="recent-projects">
 					<div class="row">
 						<div class="col">
 							<h2 class="text-center">// recent <span>projects</span></h2>
-						</div><!-- END .col -->
-					</div><!-- END .row -->
+						</div>
+					</div>
 
 					<div class="container">
 
@@ -63,21 +63,20 @@
 
 							<div class="col-md-6">
 								<div class="card">
-									<div class="card-header">// Swift Communications <span>WP News Theme</span></div>
-								  <img class="card-img-top" src="/img/work-aspen.jpg" alt="Card image cap">
+									<div class="card-header"><strong>WP News Theme</strong> - Swift Communications</div>
+								  <a href="#"><img class="card-img-top" src="/img/work-aspen-pixel.jpg" alt="Card image cap"></a>
 								  <div class="card-body">
-								    <p class="card-text">I am responsible for the co-creation and day-to-day maintenance of a custom OOP WordPress theme currently driving many popular mountain town news sites. The theme is powering over 15 sites such as Aspen Times, Vail Daily, Steamboat Today, Tahoe Daily Tribune and more.</p>
+								    <p class="card-text">I am responsible for the co-creation and day-to-day maintenance of a custom OOP WordPress theme currently driving many popular mountain town news sites. The theme is powering over 15 sites such as <a href="#">Aspen Times</a>, <a href="#">Vail Daily</a>, <a href="#">Steamboat Today</a>, <a href="#">Tahoe Daily Tribune</a> and more.</p>
 								  </div>
 								</div>
 							</div>
 
 							<div class="col-md-6">
 								<div class="card">
-									<div class="card-header">Swift Communications - WP Media Center Theme</div>
-								  <img class="card-img-top" src="/img/work-park-record.jpg" alt="Card image cap">
+									<div class="card-header"><strong>WP Media Center Theme</strong> - Swift Communications</div>
+								  <a href="#"><img class="card-img-top" src="/img/work-park-record.jpg" alt="Card image cap"></a>
 								  <div class="card-body">
-								    <h5 class="card-title">Card title</h5>
-								    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+								    <p class="card-text">News publications require a method to display content from their SmugMug account on a platform independent of the main site. This theme is running <a href="#">Park Record Photos</a>.</p>
 								  </div>
 								</div>
 							</div>
@@ -86,7 +85,159 @@
 
 					</div>
 
+				</section>-->
+
+				<div class="divider">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+				    <polygon fill="white" points="0,100 100,0 100,100"/>
+				  </svg>
+				</div>
+
+				<section id="about-me">
+
+					<div class="container">
+
+					<div class="row">
+						<div class="col">
+							<h2 class="text-center">// about <span>me</span></h2>
+						</div><!-- END .col -->
+					</div><!-- END .row -->
+
+					<div class="row no-gutters">
+						<div class="col-6">
+							<p><strong>Hi there</strong>, my name is Eric Pierpont and I am a web developer based in Steamboat Springs, CO. I grew up in Maine and attended the University of Maine where I earned my B.A. in New Media. I have been working in the industry since 2006 and have experience working for both private companies and as a contractor. I have a true passion for development which pushes me to learn something new with each and every project. It makes me smile to tell people that "I love what I do", and in the end I know that my work shows that.</p>
+
+							<p>When I am not playing web geek, you will most likely find me skiing, biking, fishing, camping, playing disc golf or any other excuse to be outside.</p>
+
+							<hr />
+
+							<div class="row">
+
+								<div class="col">
+									<h4><i class="fas fa-medal"></i> <span>my forte</span></h4>
+									<ul>
+										<li>PHP/MySQL</li>
+										<li>WordPress</li>
+										<li>HTML5/CSS3</li>
+										<li>Bootstrap</li>
+										<li>Sass</li>
+										<li>Git</li>
+										<li>JIRA</li>
+									</ul>
+								</div>
+
+								<div class="col">
+									<h4><i class="fas fa-star"></i> <span>secondary</span></h4>
+									<ul>
+										<li>PHP/MySQL</li>
+										<li>HTML5/CSS3</li>
+										<li>Bootstrap</li>
+										<li>Sass</li>
+										<li>Javascript/jQuery</li>
+										<li>Git</li>
+										<li>JIRA</li>
+									</ul>
+								</div>
+
+								<div class="col">
+									<h4><i class="fas fa-globe-americas"></i> <span>goals</span></h4>
+									<ul>
+										<li>PHP/MySQL</li>
+										<li>HTML5/CSS3</li>
+										<li>Sass</li>
+										<li>Javascript/jQuery</li>
+										<li>Git</li>
+										<li>JIRA</li>
+									</ul>
+								</div>
+
+							</div>
+
+						</div><!-- END .col -->
+						<div class="col-1"></div>
+
+
+						<div class="col-5 experience">
+
+							<div class="row no-gutters">
+								<div class="col-3 date">09.14 - Today</div>
+								<div class="col-9"><strong>Swift Communications</strong> | <span>Steamboat Springs, CO</span><br/>
+									WordPress Developer</div>
+							</div>
+
+							<div class="row no-gutters">
+								<div class="col-3 date">06.10 - 09.14</div>
+								<div class="col-9"><strong>Independent Contractor</strong> | <span>Steamboat Springs, CO</span><br/>
+									Web Developer</div>
+							</div>
+
+							<div class="row no-gutters">
+								<div class="col-3 date">10.07 - 06.10</div>
+								<div class="col-9"><strong>Resort Group</strong> | <span>Steamboat Springs, CO</span><br/>
+									Internet Marketing Specialist</div>
+							</div>
+
+							<div class="row no-gutters">
+								<div class="col-3 date">11.06 - 10.07</div>
+								<div class="col-9"><strong>Sunday River</strong> | <span>Newry, ME</span><br/>
+									Internet Marketing Specialist</div>
+							</div>
+
+							<hr />
+
+							<div class="row no-gutters">
+								<div class="col-3 date">09.02 - 05.06</div>
+								<div class="col-9"><strong>The University of Maine </strong> | <span>Orono, ME</span><br/>
+									B.A. in New Media, <em>National Dean's List Society</em></div>
+							</div>
+
+							<hr />
+
+							<a class="btn btn-primary" href="#" role="button">Resume</a>
+
+						</div>
+
+					</div><!-- END .row -->
+
+				</div>
+
 				</section>
+
+				<section id="recent-work">
+
+					<div class="row no-gutters">
+
+						<div class="col-3 text-center">
+							<div class="container d-flex h-100">
+							    <div class="row justify-content-center align-self-center w-100">
+							    	<h2>// recent <span>projects</span></h2>
+							    </div>
+							</div>
+						</div>
+
+						<div class="col">
+							<div class="card">
+								<a href="#"><img class="card-img-top" src="/img/work-vail.jpg" alt="Card image cap"></a>
+							</div>
+							<div class="card">
+								<a href="#"><img class="card-img-top" src="/img/work-vail.jpg" alt="Card image cap"></a>
+							</div>
+						</div>
+
+						<div class="col">
+							<div class="card">
+								<a href="#"><img class="card-img-top" src="/img/work-park-record.jpg" alt="Card image cap"></a>
+							</div>
+							<div class="card">
+								<a href="#"><img class="card-img-top" src="/img/work-park-record.jpg" alt="Card image cap"></a>
+							</div>
+						</div>
+
+					</div>
+
+				</section>
+
+
 
         <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
